47447b8b8af4429a02ff11617f8fb75ad7647187
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2018-01-14 02:41:14 +0300

    Start second task and setup environment

---
+++ 2_task/gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const newer = require('gulp-newer');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+// const babel = require('gulp-babel');
+
+gulp.task('styles', function () {
+	return gulp.src('./dev/styles/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('assets', function() {
+	return gulp.src(['dev/assets/**'])
+		.pipe(newer('public/assets'))
+		.pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('html', function() {
+	return gulp.src(['dev/*.html'])
+		.pipe(newer('public'))
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: 'public'
+    });
+    browserSync.watch('public/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('watch', function() {
+	gulp.watch('dev/styles/**/*.*', ['styles']).on('change', browserSync.reload);
+	gulp.watch('dev/assets/*.*', ['assets']).on('change', browserSync.reload);
+	gulp.watch('dev/*.html', ['html']).on('change', browserSync.reload);
+});
+
+gulp.task('build', ['styles', 'html', 'assets', 'watch','serve']);
